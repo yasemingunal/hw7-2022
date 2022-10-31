1d9e02546d42fd3bfce2b3bee1fde5c5f87c46f6
yasemingunal <76755092+yasemingunal@users.noreply.github.com>
2022-10-31 13:55:33 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,12 @@
+var video;// = document.querySelector(".video");
+
+window.addEventListener("load", function() {
+	// video.setItem("AUTOPLAY", false);
+	console.log("Good job opening the window");
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+});
+
+
