34f02b5e9cc9639f02f515d36044599b4144c4a1
yasemingunal <76755092+yasemingunal@users.noreply.github.com>
2022-11-01 15:51:10 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,12 +1,20 @@
-var video;// = document.querySelector(".video");
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
-	// video.setItem("AUTOPLAY", false);
 	console.log("Good job opening the window");
+	video.autoplay = false;
+	video.loop = false;
+	console.log("Autoplay is set to " + video.autoplay);
+	console.log("Loop is set to " + video.loop);
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	document.querySelector("#volume").innerHTML = video.volume *100 + "%";
+	video.play();
 });
 
-
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
