a87ca8e0345dda26524a9f8c114d7308de8c74bd
yasemingunal <76755092+yasemingunal@users.noreply.github.com>
2022-11-05 21:44:18 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -18,3 +18,39 @@ document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
 	video.pause();
 });
+
+document.querySelector("#slower").addEventListener("click", function() { 
+	video.playbackRate = video.playbackRate - 0.1;
+	console.log("Speed of video is set to " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() { 
+	video.playbackRate = video.playbackRate + 0.1;
+	console.log("Speed of video is set to " + video.playbackRate);
+
+})
+
+document.querySelector("#skip").addEventListener("click", function() { 
+	video.currentTime = video.currentTime + 10;
+	if (video.currentTime >= video.duration) { 
+		video.currentTime = 0;
+	} 
+	console.log("Current playback position of video is " + video.currentTime);
+})
+
+
+//fix this it's not working
+document.querySelector("#mute").addEventListener("click", function() { 
+	video.muted = true;
+	console.log("askjdhfalksjdhfals");
+	if (video.muted = false) { 
+		video.muted = true;
+		document.getElementById("mute").innerHtml = "unmute";
+	}
+	if (video.muted = true) { 
+		video.muted = false;
+		document.getElementById("mute").innerHtml = "mute";
+	}
+
+})
+
